591d2d4ce3e1b5336ad65e43ea7c7e1527cd45ef
better-isum-project <better.isum.project@gmail.com>
2023-02-05 23:54:31 +0100

    Initial commit

--- mail/app_mail.js
+++ mail/app_mail.js
@@ -2,39 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   const folderHeaders = document.querySelectorAll('.folder-header');
-  let currentlyOpenFolder = null;
-  
+
   folderHeaders.forEach(folderHeader => {
-    folderHeader.addEventListener("click", function(event) {
+    folderHeader.addEventListener("click", function() {
       const folderContent = this.nextElementSibling;
       const isActive = folderContent.getAttribute("active") === "true";
       folderContent.style.maxHeight = isActive ? 0 : `${folderContent.scrollHeight}px`;
       folderContent.setAttribute("active", !isActive);
       
-      let currentElement = this.parentElement;
-      while (currentElement.classList.contains("folder-content")) {
-        if (currentlyOpenFolder && folderContent !== currentlyOpenFolder) {
-          currentlyOpenFolder.style.maxHeight = 0;
-          currentlyOpenFolder.setAttribute("active", false);
-        }
-        currentElement.style.maxHeight = isActive
-          ? `${currentElement.offsetHeight - folderContent.offsetHeight}px`
-          : `${currentElement.offsetHeight + folderContent.offsetHeight}px`;
-        currentElement = currentElement.parentElement;
+      let parent = folderHeader.parentNode.parentNode;
+      while (parent) {
+        parent.style.maxHeight = 'initial';
+        parent = parent.parentNode.parentNode;
       }
-      
-      currentlyOpenFolder = isActive ? null : folderContent;
     });
   });
 
 
 
+  // toggle active attribute on a folder-header button click
 
   folderHeaders.forEach(header => {
     header.addEventListener('click', function() {
       this.classList.toggle('active');
     });
   });
+
+
+
+  // a resizable sidebar
 
   let isResizing = false;
   let startX = 0;
@@ -45,6 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
   resizeHandle.addEventListener('mousedown', function (event) {
     isResizing = true;
     startX = event.clientX;
+    console.log(scrollableDiv.scrollHeight);
+    console.log(scrollableDiv.offsetHeight);
   });
   document.addEventListener('mouseup', function () { isResizing = false; });
   document.addEventListener('mousemove', function (event) {
@@ -54,4 +52,11 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
+
+  // disable scroll bar on side bar when content doesnt exceed the height limit
+
+  const scrollableDiv = document.querySelector('#side_panel');
+  if (scrollableDiv.scrollHeight < scrollableDiv.offsetHeight) 
+    scrollableDiv.style.setProperty('-webkit-box-shadow', '#a0052f40');
+  else scrollableDiv.style.setProperty('-webkit-box-shadow', '#a0052fbf');
 });
